786184ba0febbee1e65c5f5955f2cfea4be37d34
Salim Kaboura <salimkaboura8@gmail.com>
2025-08-25 00:01:14 +0200

    fix: update unit test to use correct component reference

--- frontend/amc_app/src/app/features/user/new-order-dialog/new-order-dialog.spec.ts
+++ frontend/amc_app/src/app/features/user/new-order-dialog/new-order-dialog.spec.ts
@@ -1,18 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NewOrderDialogComponent } from './new-order-dialog';
 
-import { NewOrderDialog } from './new-order-dialog';
-
-describe('NewOrderDialog', () => {
-  let component: NewOrderDialog;
-  let fixture: ComponentFixture<NewOrderDialog>;
+describe('NewOrderDialogComponent', () => {
+  let component: NewOrderDialogComponent;
+  let fixture: ComponentFixture<NewOrderDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NewOrderDialog]
-    })
-    .compileComponents();
+      imports: [NewOrderDialogComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(NewOrderDialog);
+    fixture = TestBed.createComponent(NewOrderDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
